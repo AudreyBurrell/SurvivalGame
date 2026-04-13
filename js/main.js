fe1eb2d973488d32232f2ac4825ef7c3d29359e0
AudreyBurrell <audreyb2165@gmail.com>
2026-04-12 21:47:43 -0600

    moving around some files, adding a step by step guide for starting on the js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Steps (for now)
+/*
+    1. Initialize variables like food, people, farms, etc.
+    2 (or more like 1). Decide on whether or not I want to have a tutorial (probably yes, make sure it has a 
+    button to get out of it though and keep the variables from that as the new initializers)
+        Note: might as well keep the tutorial on main.html
+    3. Create the header displaying the information
+*/
